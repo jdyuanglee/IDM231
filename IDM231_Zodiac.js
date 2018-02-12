9dd937b874c8b1ea9109aa707878df8181cffde9
jdyuanglee <32436222+jdyuanglee@users.noreply.github.com>
2018-02-12 10:14:26 -0500

    Add Files
    js. css. html.

---
+++ IDM231_Zodiac.js
@@ -0,0 +1,25 @@
+var dob;
+var dob_month;
+var dob_day;
+var usersZodiac;
+var Zodiac = ['Rat' 'Ox' 'Tiger' 'Rabbit' 'Dragon' 'Snake' 'Horse' 'Sheep' 'Monkey' 'Rooster' 'Dog' 'Pig']
+
+function getUsersZodiac(dob, dob_month, dob_day){
+    //calculate users' Zodiac
+}
+
+function playSound(usersZodiac){
+    for (var i = 0; i < Zodiac.length; i++){
+        if(usersZodiac == Zodiac[i]{
+            //select mp3 to play base on zodiac
+        }
+    }
+}
+
+function showInfo{
+    for (var i = 0; i < Zodiac.length; i++){
+        if(usersZodiac == Zodiac[i]{
+            //select background img base on zodiac
+        }
+    }
+}
